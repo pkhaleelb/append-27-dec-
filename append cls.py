60a1ad79f619e97d52f9725f97e29f710d4facbb
pkhaleelb <pkbasha143@gmail.com>
2024-12-28 08:25:58 +0530

    Create append cls.py

---
+++ append cls.py
@@ -0,0 +1,23 @@
+#APPEND
+
+lst=(1,2,4)
+lst.append(20)
+print(lst)
+
+# extend
+
+# sales_2023=(10,20)
+# sales_2024=(1,44)
+# print(sales_2023+sales_2024)
+# sales_2023.extend(sales_2024)
+# print(sales_2023)
+
+# sales_2023=(10,20)
+# sales_2024=(1,44)
+
+#sort
+
+# val=[46664,676867,1,3,4,5,6]
+# val.sort(reverse=True)
+# print(val)
+
