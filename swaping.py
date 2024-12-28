690f57ffcf0322fe167aa3f066d90b42fe372b99
pkhaleelb <pkbasha143@gmail.com>
2024-12-28 20:47:38 +0530

    Create swaping.py

---
+++ swaping.py
@@ -0,0 +1,14 @@
+# a,b=10,20
+# a,b=b,a
+# print('a',a)
+
+# asecending order 
+
+# val=[1,87,79]
+# for x in range(len(val)):
+#     for y in range(len(val)):
+#         if val[x]>val[y]:
+#             val[x],val[y]=val[y],val[x]
+#         else:
+#             val[x],val[y]=val[x],val[y]
+# print(val)
