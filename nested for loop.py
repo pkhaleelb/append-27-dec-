97575de2e0f690f4740fa269e599f4cd110d4c21
pkhaleelb <pkbasha143@gmail.com>
2024-12-28 20:47:30 +0530

    Create nested for loop.py

---
+++ nested for loop.py
@@ -0,0 +1,44 @@
+# vertical line of nested for loop
+
+# for x in range(3):
+#     for y in range(10):
+#         print(x,y,"*")
+
+# for x in range(3):
+#     for y in range(2):
+#        print(x,y,"*",end='')
+
+# for row in range(4):
+#     for col in range(5):
+#         print("*",end=' ')
+#     print()
+
+# for row in range(4):
+#     for col in range(row+1):
+#         print("*",end=' ')
+#     print()
+
+# for row in range(5):
+#     for col in range(5-row):
+#         print("*",end=' ')
+#     print()
+
+# for row in range(5):
+#     for col in range(row+1):
+#         print("*",end=' ')
+#     print()
+# for row in range(4):
+#     for col in range(4-row):
+#         print("*",end=' ')
+#     print() 
+
+# for row in range(5):
+#     for col in range(row+1):
+#         print(row,end=' ')
+#     print()
+# for row in range(4):
+#     for col in range(row+1):
+#         print(col,end=' ')
+#     print() 
+
+ 
