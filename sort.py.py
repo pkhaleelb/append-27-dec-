c4bdcc393e90fd893fb589f0b54e8dfe3f471b9c
pkhaleelb <pkbasha143@gmail.com>
2024-12-30 14:17:36 +0530

    Create sort.py.py

---
+++ sort.py.py
@@ -0,0 +1,23 @@
+# atoms=[1,66,4,56,88,99]
+# atoms.sort()
+# print(atoms[0]+atoms[-1])
+# print(max(atoms)-min(atoms))
+
+# val=[1,3,4,5,6,7]
+# emp_lst=[]
+# for x in val:
+#     if x%2==1:
+#         emp_lst.append(x)
+#     else:
+#         pass
+# print(emp_lst)
+
+# # code optimization
+
+# 1st list comprehension
+
+# # print([x for x in[1,3,4,5,6,7]if x%2==1])
+
+# 2nd lst comprhension
+
+# print([f'even value{x}'if x%2==0 else f'{x} odd value' for x in [1,2,3]])
