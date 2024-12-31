7a18f45d8a8b8ffb990dbe8e58cdc1eec6d13a4e
pkhaleelb <pkbasha143@gmail.com>
2024-12-31 09:12:14 +0530

    Create tickets .py

---
+++ tickets .py
@@ -0,0 +1,30 @@
+#tickets
+
+# tkts=(1,2,3,4,5)
+# print('available tkts',tkts)
+# User_input=int(input('enter how many tkts u wanna buy'))
+# for x in range(User_input):
+#     tkts_num=int(input('enter tckts number:'))
+#     tkts.remove(tkts_num)
+# print('available tkts',tkts)
+
+#sets
+
+# lst=[1,2,3,4,1,2]
+# emp_lst=[]
+# for x in lst:
+#     if x not in emp_lst:
+#         emp_lst.append(x)
+# print(emp_lst)
+
+# #duplicate list 
+
+# lst=(1,2,3,4,1,2)
+# emp_lst=[]
+# dupl_lst=[]
+# for x in lst:
+#     if x in emp_lst:
+#         dupl_lst.append(x)
+#     else:
+#         emp_lst.append(x)
+# print(dupl_lst)
