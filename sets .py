d31ef561a342fadf18525f5eced4d6e76e6b1990
pkhaleelb <pkbasha143@gmail.com>
2025-01-03 07:11:36 +0530

    Create sets .py

---
+++ sets .py
@@ -0,0 +1,48 @@
+# sales_items=[]
+# print(type(sales_items))
+# val={}
+# print(type(val))
+# val=6+0.{1}
+# print(type(val))
+
+#union
+# sales_2023={1,2,3}
+# sales_2024={3,4,5}
+# print(sales_2023.union(sales_2024))
+
+# intersection
+# sales_2023={1,2,3}
+# sales_2024={3,4,5}
+# print(sales_2023.intersection(sales_2024))
+
+# difference
+# sales_2023={1,2,3}
+# sales_2024={3,4,5}
+# print(sales_2023.difference(sales_2024))
+
+# here we are getting different outputs because calling sets is different
+
+# sales_2023={1,2,3}
+# sales_2024={3,4,5}
+# print(sales_2024.difference(sales_2023))
+
+# symmetric difference
+# sales_2023={1,2,3}
+# sales_2024={3,4,5}
+# print(sales_2023.symmetric_difference(sales_2024))
+
+# sql={1,2,4,5}
+# python={1,6,7,8}
+# print(sql.intersection(python))
+# print(sql.difference(python))
+# print(sql.union(python))
+# print(sql.intersection(python))
+# print(python.difference(sql))
+# print(sql.symmetric_difference(python))
+
+# sql={1,2,4,5}
+# python={1,6,7,8}
+# aws={1,4,5,6,99}
+# print(sql.intersection(python).intersection(aws))
+# print(sql.symmetric_difference(python).symmetric_difference(aws))
+# print(python.difference(sql).difference(aws))
